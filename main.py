9f16ba26a3e6bf15ed37ac1d32c3c0b3cc501148
Daniel Wheeler <daniel.wheeler2@gmail.com>
2014-10-17 16:35:07 -0400

    App works

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from google.appengine.ext import db
 # Import the Flask Framework
 from flask import Flask, request, redirect, url_for
+import flask
 import jinja2
 import os
 
@@ -14,30 +15,43 @@ app.debug = True
 
 ## templates
 
-
 loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates'))
 env = jinja2.Environment(autoescape=True,
                          loader=loader)
-ascii_html = env.get_template('ascii.html')
-output_html = env.get_template('output.html')
+env.globals.update(url_for=url_for)
+blog_html = env.get_template('blog.html')
+submit_html = env.get_template('submit.html')
 
 
+## end points
 
-@app.route('/', methods=["GET", "POST"])
-def main_page():
-    arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+@app.route('/', methods=['GET'])
+def root():
+    return redirect(url_for('blog'))
+
+@app.route('/blog')
+def blog():
+    blogposts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC")
+    return blog_html.render(blogposts=blogposts)
+
+@app.route('/blog/<int:post_id>')
+def blog_by_id(post_id):
+    blogpost = BlogPost.get_by_id(post_id)
+    return blog_html.render(blogposts=[blogpost])
+
+@app.route('/blog/newpost', methods=['GET', 'POST'])
+def new_post():
     if request.method == 'POST':
         title = request.form["title"]
-        art = request.form["art"]
-        if title and art:
-            a = Art(title=title, art=art)
-            a.put()
-            arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
-            return redirect(url_for('main_page'))
+        post = request.form["post"]
+        if title and post:
+            entry = BlogPost(title=title, post=post)
+            entry.put()
+            return redirect(url_for('blog_by_id', post_id=entry.key().id()))
         else:
-            return ascii_html.render(art=art, title=title, error="we need both a title and some artwork!", arts=arts)
+            return submit_html.render(title=title, post=post, error="Please submit both a title and a blog post!")
     else:
-        return ascii_html.render(arts=arts)
+        return submit_html.render()
 
 
 @app.errorhandler(404)
@@ -45,9 +59,10 @@ def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, Nothing at this URL.', 404
 
+## database classes
 
-class Art(db.Model):
+class BlogPost(db.Model):
     title = db.StringProperty(required=True)
-    art = db.TextProperty(required=True)
+    post = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
     
